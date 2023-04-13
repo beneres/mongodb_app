9b8394ee8c4a2d3384ff3165e7bbebe4a98f6d0b
Bernardo Neres <90685959+beneres@users.noreply.github.com>
2023-04-13 11:18:30 +0100

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,26 @@
+import unittest
+from app import app
+
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.client = app.test_client()
+
+    def test_valid_login(self):
+        response = self.client.post('/login', data={
+            'email': 'test@example.com',
+            'password': 'password'
+        })
+        # Should redirect to logged_in page
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.headers['Location'], 'http://localhost/logged_in')
+
+    def test_invalid_login(self):
+        response = self.client.post('/login', data={
+            'email': 'test@example.com',
+            'password': 'wrong_password'
+        })
+        # Should render login page with error message
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Wrong password', response.data)
